3f8ae5a7eb7d40189937cfa4f9b6de8696214091
sushant sutar <117379840+sushantrsutar@users.noreply.github.com>
2025-02-02 16:48:17 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionCrud
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Company { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id : {Id} , Name : {Name} , Price : {Price} , Company : {Company}";
+        }
+    }
+}
---
+++ ProductCRUD.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionCrud
+{
+    public class ProductCRUD
+    {
+        private List<Product> productslist;
+
+        public ProductCRUD() { 
+            productslist = new List<Product>();
+        }
+
+        public void AddProduct(Product product) {
+            productslist.Add(product);
+        }
+        public List<Product> ShowProductList() {
+            return productslist;
+        }
+        public Product SearchById(int id) {
+            foreach (Product product in productslist) {
+                if (product.Id == id) {
+                    return product;
+                }
+            }
+            return null;
+        }
+        public bool DeleteProduct(int id) {
+            for (int i = 0; i < productslist.Count; i++) {
+                if (productslist[i].Id == id) { 
+                    productslist.RemoveAt(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool UpdateProduct(Product product)
+        {
+            for (int i = 0; i < productslist.Count; i++)
+            {
+                if (productslist[i].Id == product.Id) {
+                    productslist[i].Name= product.Name;
+                    productslist[i].Price= product.Price;
+                    productslist[i].Company= product.Company;
+                    return true;
+                }    
+            }
+            return false;
+        }
+
+    }
+}
